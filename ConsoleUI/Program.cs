caeeecca95ce7bdcf8318cae52634f679fe7f87d
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 11:33:07 -0500

    Create Code
    Created a Console Application that asks the user for their name. Welcomes Tim as professor or anyone else as a student.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			Console.Write("What is your first name: ");
+			string firstname = Console.ReadLine();
+
+			if ( firstname.ToLower() == "tim" )
+			{
+				Console.WriteLine("Hello Professor!");
+			}
+			else
+			{
+				Console.WriteLine("Hello Student!");
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
